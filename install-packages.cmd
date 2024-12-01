56a92b9ce7bb6e218c0205abbdc3748dfef82aa1
dirkarnez <smalldirkalex@gmail.com>
2024-12-01 19:22:42 +0800

    - working

--- install-packages.cmd
+++ install-packages.cmd
@@ -1,7 +1,14 @@
 @echo off
 set PYTHON_DIR=%USERPROFILE%\Downloads\python-3.10.8-amd64-portable
-set PATH=%PYTHON_DIR%;%PYTHON_DIR%\Scripts
 
-python -m pip install --upgrade pip
+set PATH=^
+%PYTHON_DIR%;^
+%PYTHON_DIR%\Scripts;^
+%~dp0.venv\Scripts;
+
+@REM echo %~dp0
+
+python -m venv .venv &&^
+.venv\Scripts\activate.bat &&^
 python -m pip install -r requirements.txt
 pause
